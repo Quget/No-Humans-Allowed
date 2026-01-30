f4faab432b3f9648f4b2c1a88266f2380ea6125a
Rose Quget <rose@quget.com>
2026-01-30 23:55:53 +0100

    Snatch to lane

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -14,12 +16,17 @@ public class Player : MonoBehaviour
     [SerializeField]
     private CameraThatFollowsATransform cameraThatFollows;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private IEnumerable<Lane> lanes;
+
+    private Lane targetLane = null;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
 		rigidbody2D = GetComponent<Rigidbody2D>();
-        cameraThatFollows.StartFollow(transform);
-	}
+        lanes = Object.FindObjectsOfType<Lane>();
+		cameraThatFollows.StartFollow(transform);
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,28 +34,49 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Dash();
+            Dash(false);
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Dash(true);
+        }
+
+        if(targetLane != null)
+        {
+            if(Mathf.Abs(transform.position.y - targetLane.transform.position.y) < 0.1f)
+            {
+                rigidbody2D.linearVelocityY = 0f;
+			}
+		}
     }
 
-	private void FixedUpdate()
+    private Lane FindLane(bool up)
+    {
+        return lanes.OrderBy(lane => up ? lane.transform.position.y - transform.position.y : transform.position.y - lane.transform.position.y)
+                    .Where( lane => lane != targetLane)
+					.FirstOrDefault(lane => up ? lane.transform.position.y > transform.position.y : lane.transform.position.y < transform.position.y);
+    }
+
+    private void FixedUpdate()
 	{
         Movement();
 	}
 
-    private void GetNearestLane(bool up)
+    private void Dash(bool up)
     {
-
+        var direction = up ? Vector2.up : Vector2.down;
+        targetLane = FindLane(up);
+        
+		if (targetLane != null)
+        {
+            rigidbody2D.AddForce(direction * forceMode, ForceMode2D.Impulse);
+        }
     }
 
-    private void Dash()
-    {
-        rigidbody2D.AddForce(Vector2.down * forceMode, ForceMode2D.Impulse);
-    }
 
-	private void Movement()
+    private void Movement()
     {
         rigidbody2D.AddForce(Vector2.right * speed,ForceMode2D.Force);
-		//rigidbody2D.MovePosition(transform.position + ((Vector3.right * speed) * Time.fixedDeltaTime));
 	}
 }
